0b25f983c1b6e9153f85206e991202244242d60e
xuyuechi <xuyuechi@m.scnu.edu.cn>
2023-02-22 11:01:26 +0800

    仗剑篇开始

---
+++ src/org/algorithms/labuladong/BTree.java
@@ -0,0 +1,7 @@
+package org.algorithms.labuladong;
+
+//仗剑篇 2.1二叉树
+public class BTree {
+
+    //leetcode 104
+}
--- src/org/algorithms/labuladong/DataStructure.java
+++ src/org/algorithms/labuladong/DataStructure.java
@@ -7,6 +7,127 @@ import java.util.*;
 //学剑篇 1.4数据结构设计
 public class DataStructure {
 
+    //leetcode 224
+    public int calculate(String s){
+        LinkedList<Character> list = new LinkedList<>();
+        for(int i=0;i<s.length();i++){
+            list.add(s.charAt(i));
+        }
+        return cal(list);
+    }
+
+    public int cal(LinkedList<Character> list){
+        Stack<Integer> results = new Stack<>();
+        char sign = '+';
+        int num = 0;
+        while(list.size()>0){
+            char c = list.pollFirst();
+            if(isDigit(c)){
+                num = 10 * num + (c-'0');
+            }
+
+            if(c=='(')
+                num = cal(list);
+
+            if((!isDigit(c) && c!=' ') || list.size() == 0){
+                if(sign == '+')
+                    results.push(num);
+                else if(sign == '-')
+                    results.push(-num);
+                else if(sign == '*')
+                    results.push(results.peek()*num);
+                else if(sign == '/')
+                    results.push(results.peek()/num);
+                num = 0;
+                sign = c;
+            }
+
+            if(c == ')')
+                break;
+        }
+        int ans =  0;
+        while(!results.isEmpty()){
+            ans += results.pop();
+        }
+        return ans;
+    }
+
+    public boolean isDigit(char c){
+        if( c - '0' < 10 && c - '0' >= 0)
+            return true;
+        else
+            return false;
+    }
+
+    //leetcode 295
+    class MedianFinder{
+
+        //大顶堆
+        PriorityQueue<Integer> small;
+
+        //小顶堆
+        PriorityQueue<Integer> large;
+        public MedianFinder(){
+            small = new PriorityQueue<>();
+            large = new PriorityQueue<>(new Comparator<Integer>() {
+                @Override
+                public int compare(Integer integer, Integer t1) {
+                    return t1 - integer;
+                }
+            });
+        }
+
+        public void addNum(int num){
+            if(small.size()>= large.size()){
+                small.offer(num);
+                large.offer(small.poll());
+            }else{
+                large.offer(num);
+                small.offer(large.poll());
+            }
+        }
+
+        public double findMedian(){
+            if(small.size() == large.size())
+                return (small.peek() + large.peek())/2.0;
+            else
+                return large.peek();
+        }
+    }
+
+    //leetcode 710
+    class AvoidBlackList{
+        int[] black;
+        HashMap<Integer,Integer> map;
+        int sz;
+        public AvoidBlackList(int n,int[] blacklist){
+            black = blacklist;
+            sz = n - blacklist.length;
+            map = new HashMap<>();
+            int last = n-1;
+            for(int i:black){
+                map.put(i,666);
+            }
+            for(int i:black){
+                while(map.containsKey(last)){
+                    last--;
+                }
+                if(i >= sz)
+                    continue;
+                map.put(i,last);
+                last--;
+            }
+        }
+
+        public int pick(){
+            int toIndex = Math.abs(new Random().nextInt(sz));
+            if(map.containsKey(toIndex)){
+                return map.get(toIndex);
+            }
+            return toIndex;
+        }
+    }
+
     //leetcode 380
     class RandomizedSet{
         ArrayList<Integer> base;
